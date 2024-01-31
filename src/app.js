d538fad1ce4c8d6c2dfc5252de2207551ad9db95
ricar <ricar@RICARDOKIRITO>
2024-01-30 23:44:33 -0400

    Updated the cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(morgan('dev'));
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://unrivaled-piroshki-7aab46.netlify.app/",
     credentials: true,
 }))
 app.use("/api", registerRoutes)
